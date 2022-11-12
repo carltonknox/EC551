ed0b3a43ab4f7c63bb8ec29abc22657d92098ce7
basilnct <57843515+basilnct@users.noreply.github.com>
2022-11-11 23:33:13 -0500

    Add files via upload

--- Lab2/Lab2.srcs/sources_1/new/fib_wrapper.v
+++ Lab2/Lab2.srcs/sources_1/new/fib_wrapper.v
@@ -25,53 +25,101 @@ module fib_wrapper(
     input ready,
     input clk,
     input rst,
+    input flag,
     output reg send,
     output reg [3:0] hex
     );
     
     // input side
-    reg [7:0] BCD;
-    reg [11:0] num_in = 12'b0;
+    reg [11:0] num_in;
     wire [5:0] n = num_in[11:8]*4'd10 + num_in[7:4];
     
     // output side
     wire print;
     wire [31:0] sum_wire;
-    reg [31:0] result = 0;
-    reg [2:0] i = 3'd7;
-    
-    always @(posedge ready)begin
-        // shift registers recording last 3 keypresses
-        num_in[11:8]<= num_in[7:4];
-        num_in[7:4]<=num_in[3:0];
-        
-        //translating ascii to bcd
+    reg [31:0] result;
+    reg [3:0] i;
+    reg check;
+
+    // input side
+    always @(negedge flag)begin
+        // shift registers recording last 3 keypresses       
+        // translating ascii to bcd
         case(ascii)
-            8'h30: num_in[3:0]<=4'h0;
-            8'h31: num_in[3:0]<=4'h1;
-            8'h32: num_in[3:0]<=4'h2;
-            8'h33: num_in[3:0]<=4'h3;
-            8'h34: num_in[3:0]<=4'h4;
-            8'h35: num_in[3:0]<=4'h5;
-            8'h36: num_in[3:0]<=4'h6;
-            8'h37: num_in[3:0]<=4'h7;
-            8'h38: num_in[3:0]<=4'h8;
-            8'h39: num_in[3:0]<=4'h9;
-            8'h0A: num_in[3:0]<=4'ha; // ENTER
+            8'h30: begin
+            num_in[11:8]<= num_in[7:4];
+            num_in[7:4]<=num_in[3:0];
+            num_in[3:0]<=4'h0;
+            end
+            8'h31: begin
+            num_in[11:8]<= num_in[7:4];
+            num_in[7:4]<=num_in[3:0];
+            num_in[3:0]<=4'h1;
+            end
+            8'h32: begin
+            num_in[11:8]<= num_in[7:4];
+            num_in[7:4]<=num_in[3:0];
+            num_in[3:0]<=4'h2;
+            end
+            8'h33: begin
+            num_in[11:8]<= num_in[7:4];
+            num_in[7:4]<=num_in[3:0];
+            num_in[3:0]<=4'h3;
+            end
+            8'h34: begin
+            num_in[11:8]<= num_in[7:4];
+            num_in[7:4]<=num_in[3:0];
+            num_in[3:0]<=4'h4;
+            end
+            8'h35: begin
+            num_in[11:8]<= num_in[7:4];
+            num_in[7:4]<=num_in[3:0];
+            num_in[3:0]<=4'h5;
+            end
+            8'h36: begin
+            num_in[11:8]<= num_in[7:4];
+            num_in[7:4]<=num_in[3:0];
+            num_in[3:0]<=4'h6;
+            end
+            8'h37: begin
+            num_in[11:8]<= num_in[7:4];
+            num_in[7:4]<=num_in[3:0];
+            num_in[3:0]<=4'h7;
+            end
+            8'h38: begin
+            num_in[11:8]<= num_in[7:4];
+            num_in[7:4]<=num_in[3:0];
+            num_in[3:0]<=4'h8;
+            end
+            8'h39: begin
+            num_in[11:8]<= num_in[7:4];
+            num_in[7:4]<=num_in[3:0];
+            num_in[3:0]<=4'h9;
+            end
+            8'h0A: begin
+            num_in[11:8]<= num_in[7:4];
+            num_in[7:4]<=num_in[3:0];
+            num_in[3:0]<=4'ha; // ENTER
+            end
         endcase
    end
    
    // fib module (rst goes low when last input is ENTER)
-    fibonacci fibonacci(.clk(clk), .rst((num_in[3:0] == 4'ha)?0:1), .n(n), .sum(sum_wire), .print(print));
+    fibonacci fibonacci(.clk(clk), .rst(rst | (num_in[3:0] != 4'ha)), .n(n), .sum(sum_wire), .print(print));
 
-   // storing fib result
-   always @(negedge print)begin
-        result <= sum_wire;
-   end
-   
-   // printing fib result
+   // output side
    always @(posedge clk) begin
-        if ((result != 0) & (ready == 1) & (i != 0)) begin
+        if (rst) begin
+            send <= 0;
+            hex <= 4'b0;
+            i <= 4'd8;
+            result <= 32'b0;
+        // store fib result
+        end else if (print) begin
+            result <= sum_wire;
+            check <= 1;
+        // print fib result
+        end else if ((check) & (ready) & (i != 0)) begin
             hex <= result[31:28];
             result[31:28]<=result[27:24];
             result[27:24]<=result[23:20];
